4939081ee1119404cf62903c31d30f69c533fd09
Chance Willmore <143205039+Arilec@users.noreply.github.com>
2026-05-22 22:35:53 -0600

    Phase 3 pathfinding movement complete
    Note to self: move AStar2D pathfinding inner class to seperate class file.

--- card-game-project/Scripts/combat.gd
+++ card-game-project/Scripts/combat.gd
@@ -1,65 +1,109 @@
 #root of all combat encounters
 extends Node2D
 
+#Player References
+@onready var player: Node2D = $Player
+@onready var player_cell = Grid.world_to_grid(player.global_position)
 
-var astar: AStarGrid2D
+#pathfinder line reference & Cell cache
+@export var pathfinder: Line2D
+var path: Array[Vector2i] = []
+var hover_cell: Vector2i
 
-#Hardcoded Obstacles
-var obstacles: Array[Vector2i] = [
-	Vector2i(3, 0),
-	Vector2i(3, 1),
-	Vector2i(3, 2),
-	Vector2i(3, 3),
-	Vector2i(3, 4),
-]
-
+#A* initiation
+var astar: AStar
 func _ready() -> void:
-	setup_pathfinding()
-	test_pathfinding()
+	astar = AStar.new()
 
-#creates and defines our AStarGrid2D
-func setup_pathfinding() -> void:
-	#instantiate AStarGrid2D
-	astar = AStarGrid2D.new()
-	
-	#region of valid cells
-	astar.region = Rect2i(Vector2i.ZERO, Grid.SIZE)
-	astar.cell_size = Grid.CELL_SIZE
-	
-	#diagonal movement?
-	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
-	
-	#update must be called after configuration
-	astar.update()
-	
-	for obstacle in obstacles:
-		astar.set_point_solid(obstacle, true)
-		
-		
-#temp function to test pathfinding
-func test_pathfinding() -> void:
-	var path: Array[Vector2i] = astar.get_id_path(Vector2i(2, 3), Vector2i(4, 2))
-	print("path from (2, 3) to (4, 2):")
-	for cell in path:
-		print(" ", cell)
-	print("total cells: ", path.size())
-	
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	queue_redraw()
 	
-# Checks if the mouse moves. prints the relative grid position of the mouse
-"""
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		var cell = Grid.world_to_grid(get_global_mouse_position())
-		if Grid.is_within_grid(cell):
-			print("Hovering Cell: ", cell)
-"""
 #draws the grid		
 func _draw() -> void:
 	for x in Grid.SIZE.x:
 		for y in Grid.SIZE.y:
 			var rect = Rect2(Vector2(x, y) * Grid.CELL_SIZE, Grid.CELL_SIZE)
 			draw_rect(rect, Color.BLACK, false, 2.0)
+
+enum State { IDLE, CHARACTER_SELECTED }
+
+var state = State.IDLE
+
+#general input handler
+func _input(event: InputEvent) -> void:
+	#sends input to proper channels based on input
+	match state:
+		State.IDLE:
+			_input_IDLE(event)
+		State.CHARACTER_SELECTED:
+			_input_CHARACTER_SELECTED(event)
+
+#input handler for IDLE State
+func _input_IDLE(event: InputEvent) -> void:
+	if event is InputEventMouseButton && event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
+		if Grid.world_to_grid(get_global_mouse_position()) == player_cell:
+			change_state(State.CHARACTER_SELECTED)
+
+#input handler for CHARACTER_SELECTED State
+func _input_CHARACTER_SELECTED(event: InputEvent) -> void:
+	if event is InputEventMouseButton && event.is_pressed():
+		match event.button_index:
+			MOUSE_BUTTON_RIGHT:
+				change_state(State.IDLE)
+	elif event is InputEventMouseMotion:
+		hover_cell = Grid.world_to_grid(get_global_mouse_position())
+		if (Grid.is_within_grid(hover_cell)):
+			path = astar.pathfinder_logic.get_id_path(player_cell, hover_cell)
+			pathfinder.clear_points()
+			for cell in path:
+				pathfinder.add_point(Grid.grid_to_world(cell))
+		
+
+#State Machine Mechanical Heart
+func change_state(new_state: State):
+	#exit State
+	match state:
+		State.CHARACTER_SELECTED:
+			pathfinder.clear_points()
+			path.clear()
+	
+	#enter State
+	state = new_state
+
+#the entirity of the A*grid implementation
+class AStar:
+	
+	var pathfinder_logic: AStarGrid2D
+
+	#Hardcoded Obstacles
+	var obstacles: Array[Vector2i] = [
+		Vector2i(3, 0),
+		Vector2i(3, 1),
+		Vector2i(3, 2),
+		Vector2i(3, 3),
+		Vector2i(3, 4),
+	]
+	
+	#runs on initialization
+	func _init():
+		setup_pathfinding()
+			
+	#creates and defines our AStarGrid2D
+	func setup_pathfinding() -> void:
+		#instantiate AStarGrid2D
+		pathfinder_logic = AStarGrid2D.new()
+		
+		#region of valid cells
+		pathfinder_logic.region = Rect2i(Vector2i.ZERO, Grid.SIZE)
+		pathfinder_logic.cell_size = Grid.CELL_SIZE
+		
+		#diagonal movement?
+		pathfinder_logic.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+		
+		#update must be called after configuration
+		pathfinder_logic.update()
+		
+		for obstacle in obstacles:
+			pathfinder_logic.set_point_solid(obstacle, true)	
